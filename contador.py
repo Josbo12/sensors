06ca8cc252815caa225e7713d6c3874edac95509
Josbo12 <josepborras8@gmail.com>
2016-05-13 17:40:36 +0200

    res

--- contador.py
+++ contador.py
@@ -13,6 +13,9 @@ class FlowControl(object):
         self.count = 0
         self.litres = 0
         self.litres_decimal = 0
+        self.service = 0
+        self.total = 0
+        
     def _get_user(self):
         print "llegeixo nfc"
 
