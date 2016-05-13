1965761a9f33e5ef8e3ad792df887251a6451ee9
Josbo12 <josepborras8@gmail.com>
2016-05-13 17:43:57 +0200

    canviada la forma de mesura flux

--- contador.py
+++ contador.py
@@ -15,7 +15,7 @@ class FlowControl(object):
         self.litres_decimal = 0
         self.service = 0
         self.total = 0
-        
+
     def _get_user(self):
         print "llegeixo nfc"
 
@@ -23,11 +23,16 @@ class FlowControl(object):
         tim = time.time()
         delta = tim - self.previousTime
         if delta < 500:
-            self.count = self.count+1
-            self.litres =(self.count/450.0)
-            service = '%.3f' % self.litres
-            self.service += service
-            self.total += service
+            #self.count = self.count+1
+            #self.litres =(self.count/450.0)
+            #service = '%.3f' % self.litres
+            #self.service += service
+            #self.total += service
+            self.hertz = 1000.0 / delta
+            self.flow = self.hertz / 450.0  # Liter/Second
+            service = self.flow * (delta / 1000.0)
+            self.service  += service
+            self.total    += service
         else:
             self.service = 0
             self.user = self._get_user()
